dc8bde5a49fea64fa6b0d6b235093daf66d17b20
rafsig <rafsig@hotmail.com>
2021-10-13 03:43:57 -0400

    test

--- src/main/java/com/sigwalt/priorityQueue/model/PriorityQueue.java
+++ src/main/java/com/sigwalt/priorityQueue/model/PriorityQueue.java
@@ -8,6 +8,7 @@ public class PriorityQueue <T>{
 	
 	private Map<Integer, List<QueueItem<T>>> queue;
 	private int maxSize;
+	private int test;
 	
 	public PriorityQueue(int maxSize){
 		this.queue = new HashMap<Integer, List<QueueItem<T>>>();
